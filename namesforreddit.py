1c419d9205764d67a7f6136b7a4a324c53f377ec
Marie Montgomery <marielmont@users.noreply.github.com>
2022-04-02 20:52:41 +1100

    Changed deprecated code

--- namesforreddit.py
+++ namesforreddit.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import random
 import time
 import re
@@ -18,7 +21,7 @@ password = ''.join(secrets.choice(alphabet) for i in range(16))
 
 # NAME GENERATION
 driver.get('https://en.wikipedia.org/wiki/Special:Random')
-temp = driver.find_element_by_class_name("firstHeading").text
+temp = driver.find_element(By.CLASS_NAME, "firstHeading").text
 for char in string.punctuation:
     temp = temp.replace(char, '') #REMOVES ALL PUNCTUATION
 for char in string.digits:
@@ -43,11 +46,13 @@ time.sleep(1)
 
 # REDDIT ACCOUNT CREATION
 driver.get('https://www.reddit.com/register/')
-driver.find_element_by_id('regEmail').send_keys('mail@mail.mail')
+driver.find_element(By.ID, 'regEmail').send_keys('mail@mail.mail')
 time.sleep(1)
-driver.find_element_by_xpath ("//button[contains(text(),'Continue')]").click()
+driver.find_element(By.XPATH, "//button[contains(text(),'Continue')]").click()
 time.sleep(3)
-driver.find_element_by_id('regUsername').send_keys(finalName)
-driver.find_element_by_id('regPassword').send_keys(password)
+driver.find_element(By.ID, 'regUsername').send_keys(finalName)
+driver.find_element(By.ID, 'regPassword').send_keys(password)
+
+WebDriverWait(driver, 40).until(EC.visibility_of_element_located((By.CLASS_NAME, "recaptcha-checkbox-checkmark")))
 
 # driver.close()
